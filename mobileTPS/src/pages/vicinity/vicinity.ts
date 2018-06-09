2a17fd14aa70fa3a10f7af6cea668ea69ed3d27a
Keena Shang <keena.shang@mail.utoronto.ca>
2018-06-09 15:13:41 -0400

    meep

--- mobileTPS/src/pages/vicinity/vicinity.ts
+++ mobileTPS/src/pages/vicinity/vicinity.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Http} from '@angular/http'
 
 @Component({
   selector: 'page-vicinity',
   templateUrl: 'vicinity.html'
 })
 export class VicinityPage {
+  lat: number;
+  lng: number;
+  dist_from_you: any;
+  emerg_type: any
+  time: any;
+  maj_int: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private http: Http, public navCtrl: NavController) {
 
+}
+
+  ionViewWillEnter() {
+    this.http.post('https://109dcaa9.ngrok.io/incidents', {
+      'lat': this.lat,
+      'lng': this.lng
+    }).toPromise().then(data => {
+      this.incidents(data.json())
+    }).catch(error => {
+      console.error('An error occurred in HomePage', error);
+      return Promise.reject(error.message || error);
+    });
+  }
+
+  incidents(args: any) {
+    this.dist_from_you = args['dist_from_you'];
+    this.emerg_type = args['emerg_type'];
+    this.time = args['time'];
+    this.maj_int = args['maj_int'];
   }
 
 }
