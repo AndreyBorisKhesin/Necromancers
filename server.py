b6a5939aaa5a1ca3797244d4f6b72953e64ed632
Andrey Boris Khesin <andrey.khesin@gmail.com>
2018-06-09 15:22:57 -0400

    Server Distance Calculations.
    Now, we can track the events nearest to the user.
    And it works pretty well.
    Please ignore the wall of debug statements.

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import datetime
 from flask import Flask, request, redirect, jsonify
 from flask_cors import CORS
 from json import loads
+import math
 import requests
 import twilio.twiml
 from twilio.twiml.messaging_response import MessagingResponse
@@ -13,7 +14,7 @@ CORS(app)
 
 database = set()
 
-def minkowsky_distance(loc, event):
+def distance(loc, event):
 	x_loc = loc[0]
 	y_loc = loc[1]
 	t_loc = datetime.datetime.now()
@@ -25,7 +26,7 @@ def minkowsky_distance(loc, event):
 	# print(sq(y_loc - y_event))
 	# print(sq((t_loc - t_event).total_seconds()/60/60/100))
 	# print(1/0)
-	return sq(x_loc - x_event) + sq(y_loc - y_event) # + sq((t_loc - t_event).total_seconds()/60/60/100)
+	return math.sqrt(sq((x_loc - x_event) * 1000 / 9) + sq((y_loc - y_event) * 80.3923585722))
 
 @app.route('/', methods = ['POST'])
 def root():
@@ -36,19 +37,23 @@ def root():
 @app.route('/incidents', methods = ['POST'])
 def incidents():
 	data = loads(request.data.decode('utf-8'))
-	# vlist = sorted(map(lambda x: (minkowsky_distance((43.657771,-79.381107),x), x), list(database)))
-	"""return jsonify(map(lambda x: {
-		'dist_from_you':math.sqrt(x[0]),
-		'emerg_type': x[1][0],
-		'time': str(x[1][1]),
-		'maj_int': 'NULL'},
-	vlist[:5]))"""
-	return jsonify({
-		'dist_from_you': 2.1,
-		'emerg_type': 'Breaking and Entering',
-		'time': '12:56 PM',
-		'maj_int': 'Yonge and Dundas',
-	})
+	vlist = sorted(map(lambda x: (distance((data['lat'],data['lng']),x) + (datetime.datetime.now() - x[1]).total_seconds() / 18000,distance((data['lat'],data['lng']),x), x), list(database)))
+	if len(vlist) != 0:
+		print((data['lat'],data['lng']))
+		print(vlist)
+		return jsonify((lambda x: {
+			'dist_from_you':x[1],
+			'emerg_type': x[2][0],
+			'time': str(x[2][1]),
+			'maj_int': 'NULL'})(vlist[0]))
+		# vlist[:5])))))
+	else:
+		return jsonify({
+			'dist_from_you': 2.1,
+			'emerg_type': 'Breaking and Entering',
+			'time': '12:56 PM',
+			'maj_int': 'Yonge and Dundas',
+		})
 
 def parse_event(event):
 	type = event['attributes']['TYP_ENG']
@@ -69,7 +74,7 @@ def scrape():
 			if event not in database:
 				print(event)
 				database.add(event)
-		# vlist = sorted(map(lambda x: (minkowsky_distance((43.657771,-79.381107),x), x), list(database)))
+		# vlist = sorted(map(lambda x: (distance((43.657771,-79.381107),x), x), list(database)))
 
 @app.before_first_request
 def init_scraper():
