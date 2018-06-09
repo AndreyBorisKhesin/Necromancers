ca9c6af97b329a14bf369dab3d6a20ecd94f8253
Andrey Boris Khesin <andrey.khesin@gmail.com>
2018-06-09 11:35:41 -0400

    made responses work

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, jsonify
+from flask_cors import CORS
 from json import loads
 import twilio.twiml
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods = ['POST'])
 def root():
@@ -15,7 +17,12 @@ def root():
 @app.route('/incidents', methods = ['POST'])
 def incidents():
 	data = loads(request.data.decode('utf-8'))
-	return
+	return jsonify({
+		'dist_from_you': 2.1,
+		'emerg_type': 'Breaking and Entering',
+		'time': '12:56 PM',
+		'maj_int': 'Yonge and Dundas',
+	})
 
 if __name__ == "__main__":
     app.run()
