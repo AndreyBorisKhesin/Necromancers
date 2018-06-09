99f6bc574adf9721c4ba4ea832228809f1c479e7
doome1337 <dmitry.paramonov@gmail.com>
2018-06-09 13:46:31 -0400

    Trying to sort to find the most priority events.

--- server/server.py
+++ server/server.py
@@ -11,6 +11,22 @@ from twilio.rest import Client
 app = Flask(__name__)
 CORS(app)
 
+database = set()
+
+def minkowsky_distance(loc, event):
+    x_loc = loc[0]
+    y_loc = loc[1]
+    t_loc = datetime.datetime.now()
+    x_event = event[2]
+    y_event = event[3]
+    t_event = event[1]
+    sq = lambda x: x*x
+    # print(sq(x_loc - x_event))
+    # print(sq(y_loc - y_event))
+    # print(sq((t_loc - t_event).total_seconds()/60/60/100))
+    # print(1/0)
+    return sq(x_loc - x_event) + sq(y_loc - y_event) + sq((t_loc - t_event).total_seconds()/60/60/100)
+
 @app.route('/', methods = ['POST'])
 def root():
 	resp = MessagingResponse()
@@ -35,18 +51,25 @@ def parse_event(event):
     return (type, time, lat, long)
 
 def scrape():
-    url = "http://c4s.torontopolice.on.ca/arcgis/rest/services/CADPublic/C4S/MapServer/0/query?f=json&where=1%3d1&outfields=*&outSR=4326"
-    results = loads(requests.post(url).text)
-    for result in results['features']:
-        # print(parse_event(result))
-        break
+    global database
+    with app.app_context():
+        url = "http://c4s.torontopolice.on.ca/arcgis/rest/services/CADPublic/C4S/MapServer/0/query?f=json&where=1%3d1&outfields=*&outSR=4326"
+        results = loads(requests.post(url).text)
+        print("--------")
+        print(datetime.datetime.now())
+        for result in results['features']:
+            event = parse_event(result)
+            if event not in database:
+                print(event)
+                database.add(event)
+        # vlist = sorted(map(lambda x: (minkowsky_distance((43.657771,-79.381107),x), x), list(database)))
 
 @app.before_first_request
 def init_scraper():
     apsched = BackgroundScheduler()
     apsched.start()
 
-    apsched.add_job(scrape, trigger="interval", seconds=60)
+    apsched.add_job(scrape, trigger="interval", minutes=1)
 
 if __name__ == "__main__":
     app.run()
